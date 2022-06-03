2600b9ce192a8cabff9ec887d4ebed65342f14fd
Ramón Felix <rfelix@sigma-alimentos.com>
2022-06-02 19:14:10 -0500

    Mi primera versión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+import { PaisModule } from './pais/pais.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule, PaisModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
