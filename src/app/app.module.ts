aaf3951e123ab33589ae54dc3b401b57967f638a
Ramón Felix <rfelix@sigma-alimentos.com>
2022-06-03 19:45:37 -0500

    Visualizar datos de los paises en pantalla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
-import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
-import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
-import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
 import { PaisModule } from './pais/pais.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, PaisModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    PaisModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,11 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class PorPaisComponent implements OnInit {
   termino: string = '';
 
-  constructor() {}
+  hayError: boolean = false;
+
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) {}
 
   ngOnInit(): void {}
 
   buscar() {
+    this.hayError = false;
     console.log(this.termino);
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        this.paises = paises;
+      },
+      (error) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
 }
