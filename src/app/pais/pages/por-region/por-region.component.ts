d7cc5a551d61b046d50e638dd10b90ebd109828c
Ramón Felix <rfelix@sigma-alimentos.com>
2022-06-08 16:31:27 -0500

    Busqueda por región y optimización de peticiones

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styleUrls: ['./por-region.component.css']
+  styleUrls: ['./por-region.component.css'],
 })
 export class PorRegionComponent implements OnInit {
+  regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getClaseCSS(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
   }
 
+  activarRegion(region: string) {
+    if (region === this.regionActiva) {
+      return;
+    }
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService
+      .buscarRegion(region)
+      .subscribe((paises) => (this.paises = paises));
+  }
 }
